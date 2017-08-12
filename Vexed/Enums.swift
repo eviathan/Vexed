45ea05052367476f8d0e1d0d4319db22614cfcf5
Brian Williams <email.brianwilliams.gmail.com>
2017-08-12 22:58:50 +0100

    Added Striped Flags

--- Vexed/Enums.swift
+++ Vexed/Enums.swift
@@ -11,6 +11,52 @@ import Foundation
 enum FlagType {
     case SolidFlag
     case CrossFlag
+    case StripeFlag
+}
+
+enum Orientation: UInt32 {
+    case Horizontal
+    case Vertical
+    
+    private static let _count: Orientation.RawValue = {
+        var maxValue: UInt32 = 0
+        while let _ = Orientation(rawValue: maxValue) {
+            maxValue += 1
+        }
+        return maxValue
+    }()
+    
+    static func random() -> Orientation {
+        let rand = arc4random_uniform(_count)
+        return Orientation(rawValue: rand)!
+    }
+}
+
+enum PalletMode {
+    case Free
+    case FreeHue
+    case Complimentary
+    case Primary
+}
+
+enum ColorMode: UInt32 {
+    case Unique
+    case Alternating
+    case Rainbow
+    
+    private static let _count: ColorMode.RawValue = {
+        var maxValue: UInt32 = 0
+        while let _ = ColorMode(rawValue: maxValue) {
+            maxValue += 1
+        }
+        return maxValue
+    }()
+    
+    static func random() -> ColorMode {
+        let rand = arc4random_uniform(_count)
+        let colorMode = ColorMode(rawValue: rand)!
+        return colorMode == .Rainbow ? .Unique : colorMode // NOTE: Disable Rainbow mode until preferences implemented
+    }
 }
 
 enum BackgroundType {
--- Vexed/StripeFlag.swift
+++ Vexed/StripeFlag.swift
@@ -10,7 +10,98 @@ import Foundation
 import UIKit
 
 class StripeFlag: Flag {
+    
+    var MIN_STRIPES: UInt32 {
+        get {
+            return colorMode == .Rainbow ? 5 : 2
+        }
+    }
+    
+    var  MAX_STRIPES: UInt32 {
+        get {
+            return orientation == .Horizontal ? 5 : 15
+        }
+    }
+    
+    var amountOfStripes: Int = 2;
+    var amountOfColours: Int = 2;
+    var orientation: Orientation = Orientation.random()
+    
+    var colorMode: ColorMode = .Alternating
+    var colors: [UIColor] = [UIColor]()
+    
+    init(bypass: Bool = false) {
+        if(!bypass){
+            
+            amountOfStripes = Int(arc4random_uniform(MAX_STRIPES - MIN_STRIPES) + MIN_STRIPES)
+            let randomColorMode = ColorMode.random()
+            colorMode = amountOfStripes > 4 ? .Alternating : randomColorMode
+            amountOfColours = getAmountOfColors()
+            
+            for i in 0...amountOfColours {
+                if colorMode == .Rainbow {
+                    let hue: CGFloat = CGFloat(i) / CGFloat(amountOfColours)
+                    colors.append(UIColor(hue: hue, saturation: 1.0, brightness: 1.0, alpha: 1.0))
+                } else {
+                    colors.append(.random())
+                }
+            }
+        }
+    }
+    
     func drawFlag(rect: CGRect) -> CALayer {
-        return CALayer()
+        let layer = CAShapeLayer()
+        
+        for i in 0...amountOfStripes {
+            layer.addSublayer(createStripe(rect: rect, index: i))
+        }
+        
+        return layer
+    }
+    
+    func createStripe(rect: CGRect, index: Int) -> CAShapeLayer {
+        
+        let stripe = CAShapeLayer()
+        let stripeRect = getCurrentStripeRect(rect: rect, index: index)
+        
+        stripe.path = UIBezierPath(rect: stripeRect).cgPath
+        stripe.fillColor = getCurrentColor(index: index).cgColor
+        
+        return stripe
+    }
+    
+    private func getAmountOfColors() -> Int {
+        switch colorMode {
+        case .Alternating:
+            return 2;
+        case .Unique:
+            fallthrough
+        case .Rainbow:
+            return amountOfStripes
+        }
+    }
+    
+    func getCurrentStripeRect(rect: CGRect, index: Int) -> CGRect {
+        
+        let width = rect.width / CGFloat(amountOfStripes)
+        let height = rect.height / CGFloat(amountOfStripes)
+        
+        switch orientation {
+        case .Horizontal:
+            return CGRect(x: 0, y: height * CGFloat(index), width: rect.width, height: height)
+        case .Vertical:
+            return CGRect(x: width * CGFloat(index), y: 0, width: width, height: rect.height)
+        }
+    }
+    
+    func getCurrentColor(index: Int) -> UIColor {
+        switch colorMode {
+        case .Alternating:
+            return colors[index % 2]
+        case .Unique:
+            fallthrough
+        case .Rainbow:
+            return colors[index]
+        }
     }
 }
