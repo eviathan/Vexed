26edc64f30f810bd0a74a291ebf0a015149b795b
Brian Williams <email.brianwilliams.gmail.com>
2017-08-13 03:20:58 +0100

    Add Composite flag class

---
+++ Vexed/CompositeFlag.swift
@@ -0,0 +1,16 @@
+//
+//  CompositeFlag.swift
+//  Vexed
+//
+//  Created by Brian Williams on 13/08/2017.
+//  Copyright © 2017 Brian Williams. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CompositeFlag: Flag {
+    func draw(rect: CGRect) -> CALayer {
+        return CAShapeLayer()
+    }
+}
